cf130dd13a9d3a1da394f094401534c0256de665
Samee Siddiqui <sameezsiddiqui@gmail.com>
2016-09-27 01:21:39 -0400

    pre rezzy setup

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 var backgrounds = [
-  "url('img/hq/facebook_hq.jpg')",
-  "url('img/hq/google_hq.jpg')",
-  "url('img/hq/airbnb_hq.jpg')",
-  "url('img/hq/microsoft_hq.jpg')",
-  "url('img/hq/tesla_hq.jpg')"
-];
 
+  "url('img/hq/google_hq.jpg')",
+  "url('img/hq/apple_hq.jpg')",
+  "url('img/hq/microsoft_hq.jpg')",
+  "url('img/hq/facebook_hq.jpg')",
+  "url('img/hq/twitter_hq.jpg')",
+  "url('img/hq/facebook_hq2.jpg')"
+
+];
+var check = true;
 var slideIndex =-1;
 auto();
 
 function auto() {
-  carousel(slideIndex +=1);
+  if (check) {
+  slideIndex+=1;
+  carousel();
   setTimeout(auto, 8000);
+}
 }
 
 function currentDiv(n) {
-  carousel(slideIndex=n);
+  if (check) {
+  slideIndex = n;
+  carousel();
+}
 }
 
-function carousel(num) {
+function carousel() {
   var i;
+  check = false;
   var x = document.getElementsByClassName("imageselect");
-  if (num>=backgrounds.length){slideIndex=0}
+
+  if (slideIndex>=backgrounds.length){slideIndex=0}
 
   for (i=0; i< x.length; i++) {
     x[i].className = x[i].className.replace(" whitehover", "");
   }
 
   x[slideIndex].className += " whitehover";
+
   document.getElementsByClassName("header")[0].style.background = backgrounds[slideIndex];
   document.getElementsByClassName("header")[0].style.backgroundSize = "cover";
   document.getElementsByClassName("header")[0].style.backgroundPosition = "center";
+  setTimeout(function(){
+    check = true;
+  }, 500);
 }
